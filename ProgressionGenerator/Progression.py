08dade029ca7ee653092db98dfad37593a5edb0e
Justin Elliott <justin@elliott.cc>
2023-11-30 21:27:18 -0500

    Add subclass progressions.

--- ProgressionGenerator/Progression.py
+++ ProgressionGenerator/Progression.py
@@ -8,7 +8,7 @@ import uuid
 
 import xml.etree.ElementTree as ElementTree
 
-from typing import AnyStr, Final, Pattern
+from typing import AnyStr, Final, NamedTuple, Pattern
 
 CLASS_NAMES: Final[set[str]] = {
     "Barbarian",
@@ -27,12 +27,12 @@ CLASS_NAMES: Final[set[str]] = {
 
 def class_list(s: str) -> set[str]:
     classes = set([t.title() for t in s.split(",")])
-    if classes & CLASS_NAMES != classes:
+    if classes & CLASS_NAMES != classes or len(classes) == 0:
         raise "Invalid class names"
-    return classes if len(classes) > 0 else CLASS_NAMES
+    return classes
 
 parser = argparse.ArgumentParser(description='Combine the Progression.lsx tables, updating feat and resource definitions')
-parser.add_argument("-c", "--classes", type=class_list, default=set(),
+parser.add_argument("-c", "--classes", type=class_list, default=CLASS_NAMES,
                     help="Classes to include in the progression (defaulting to all)")
 parser.add_argument("-f", "--feats", type=int, choices=range(1,5), default=4,
                     help="Feat progression every n levels (defaulting to 4; normal progression)")
@@ -49,18 +49,63 @@ ACTION_RESOURCES: Final[list[str]] = [
     "BardicInspiration",
     "ChannelDivinity",
     "ChannelOath",
+    "FungalInfestationCharge",
     "KiPoint",
     "LayOnHandsCharge",
+    "NaturalRecoveryPoint",
     "Rage",
     "SorceryPoint",
     "SpellSlot",
+    "SuperiorityDie",
     "WarlockSpellSlot",
+    "WarPriestActionPoint",
+    "WildShape",
 ]
 SPELL_SLOT_REGEX: Final[Pattern[AnyStr]] = re.compile(f"ActionResource\\(({"|".join(ACTION_RESOURCES)}),\\s*(\\d+),\\s*(\\d+)\\)")
 
-def collect_nodes(root: ElementTree) -> dict[tuple[str, int, str], ElementTree.Element]:
-    nodes: dict[tuple[str, int, bool], ElementTree.Element] = {}
-    for node in root.findall("./region[@id='Progressions']/node[@id='root']/children/node[@id='Progression']"):
+UuidToClassDict = dict[str, str]
+SubclassToClassDict = dict[str, str]
+
+class ProgressionsKey(NamedTuple):
+    class_name: str
+    subclass_name: str
+    level: int
+    uuid: str
+
+ProgressionsDict = dict[ProgressionsKey, ElementTree.Element]
+
+def collect_uuid_to_class(class_descriptions: ElementTree) -> UuidToClassDict:
+    uuid_to_class: UuidToClassDict = {}
+    for node in class_descriptions.findall("./region[@id='ClassDescriptions']/node[@id='root']/children/node[@id='ClassDescription']"):
+        uuid_node = node.find("attribute[@id='UUID']")
+        name_node = node.find("attribute[@id='Name']")
+        if uuid_node != None and name_node != None:
+            uuid = uuid_node.get("value")
+            name = name_node.get("value")
+            if uuid != None and name != None:
+                uuid_to_class[uuid] = name
+    return uuid_to_class
+
+def collect_subclass_to_class(progressions: ElementTree, uuid_to_class: UuidToClassDict) -> SubclassToClassDict:
+    subclass_to_class: SubclassToClassDict = dict()
+    for node in progressions.findall("./region[@id='Progressions']/node[@id='root']/children/node[@id='Progression']"):
+        name_node = node.find("attribute[@id='Name']")
+        children_node = node.find("children")
+        if name_node != None and children_node != None:
+            name = name_node.get("value")
+            if name in args.classes:
+                subclass_nodes = children_node.findall("node[@id='SubClasses']/children/node[@id='SubClass']")
+                for subclass_node in subclass_nodes:
+                    class_description_node = subclass_node.find("attribute[@id='Object']")
+                    if class_description_node != None:
+                        uuid = class_description_node.get("value")
+                        if uuid in uuid_to_class:
+                            subclass_to_class[uuid_to_class[uuid]] = name
+    return subclass_to_class
+
+def collect_progressions(progressions: ElementTree, subclass_to_class: SubclassToClassDict) -> ProgressionsDict:
+    nodes: ProgressionsDict = {}
+    for node in progressions.findall("./region[@id='Progressions']/node[@id='root']/children/node[@id='Progression']"):
         name_node = node.find("attribute[@id='Name']")
         level_node = node.find("attribute[@id='Level']")
         uuid_node = node.find("attribute[@id='UUID']")
@@ -68,12 +113,15 @@ def collect_nodes(root: ElementTree) -> dict[tuple[str, int, str], ElementTree.E
             name = name_node.get("value")
             level = level_node.get("value")
             uuid = uuid_node.get("value")
-            if name in args.classes and level != None and uuid != None:
-                nodes[(name, int(level), uuid)] = node
+            if level != None and uuid != None:
+                if name in args.classes:
+                    nodes[(name, "", int(level), uuid)] = node
+                elif name in subclass_to_class:
+                    nodes[(subclass_to_class[name], name, int(level), uuid)] = node
     return nodes
 
-def feat_every_n_levels(nodes: dict[tuple[str, int, str], ElementTree.Element], n_levels: int):
-    for (_, level, _), node in nodes.items():
+def feat_every_n_levels(nodes: ProgressionsDict, n_levels: int):
+    for (_, _, level, _), node in nodes.items():
         allow_improvement_node = node.find("attribute[@id='AllowImprovement']")
         allow_improvement = (level > 1 and level % n_levels == 0) if allow_improvement_node == None else (
             allow_improvement_node.get("value").lower() == "true")
@@ -82,7 +130,7 @@ def feat_every_n_levels(nodes: dict[tuple[str, int, str], ElementTree.Element], 
         if allow_improvement:
             ElementTree.SubElement(node, "attribute", attrib={"id": "AllowImprovement", "type": "bool", "value": "true"})
 
-def action_resources_multiplier(nodes: dict[tuple[str, int, str], ElementTree.Element], multiplier: int):
+def action_resources_multiplier(nodes: ProgressionsDict, multiplier: int):
     for node in nodes.values():
         boosts_node = node.find("attribute[@id='Boosts']")
         if boosts_node != None:
@@ -91,7 +139,7 @@ def action_resources_multiplier(nodes: dict[tuple[str, int, str], ElementTree.El
                 boosts = SPELL_SLOT_REGEX.sub(lambda match: f"ActionResource({match[1]},{int(match[2])*multiplier},{match[3]})", boosts)
                 boosts_node.set("value", boosts)
 
-def sort_node_attributes(nodes: dict[tuple[str, int, str], ElementTree.Element]):
+def sort_node_attributes(nodes: ProgressionsDict):
     for node in nodes.values():
         attributes = node.findall("attribute[@id]")
         sorted_attributes = sorted([(attribute.get("id"), attribute) for attribute in attributes])
@@ -104,31 +152,45 @@ def sort_node_attributes(nodes: dict[tuple[str, int, str], ElementTree.Element])
             node.remove(children)
             node.append(children) # Move to back
 
+shared_class_descriptions_file: Final[str] = os.path.join(UNPACKED_MODS_DIR, "Shared", "Public", "Shared", "ClassDescriptions", "ClassDescriptions.lsx")
+shareddev_class_descriptions_file: Final[str] = os.path.join(UNPACKED_MODS_DIR, "Shared", "Public", "SharedDev", "ClassDescriptions", "ClassDescriptions.lsx")
+
+shared_class_descriptions = ElementTree.parse(shared_class_descriptions_file)
+shareddev_class_descriptions = ElementTree.parse(shareddev_class_descriptions_file)
+
+uuid_to_class = collect_uuid_to_class(shared_class_descriptions) | collect_uuid_to_class(shareddev_class_descriptions)
+
 shared_progressions_file: Final[str] = os.path.join(UNPACKED_MODS_DIR, "Shared", "Public", "Shared", "Progressions", "Progressions.lsx")
 shareddev_progressions_file: Final[str] = os.path.join(UNPACKED_MODS_DIR, "Shared", "Public", "SharedDev", "Progressions", "Progressions.lsx")
 
 shared_progressions = ElementTree.parse(shared_progressions_file)
 shareddev_progressions = ElementTree.parse(shareddev_progressions_file)
 
-combined_nodes = collect_nodes(shared_progressions) | collect_nodes(shareddev_progressions)
+subclass_to_class = collect_subclass_to_class(shared_progressions, uuid_to_class) | collect_subclass_to_class(shareddev_progressions, uuid_to_class)
+
+combined_progressions = collect_progressions(shared_progressions, subclass_to_class) | collect_progressions(shareddev_progressions, subclass_to_class)
 
 # Remove the unused (and unusual) Cleric progression entry
-UNUSED_CLERIC_KEY: Final[tuple[str, int, str]] = ("Cleric", 1, "2b249feb-bba5-4922-8385-c2dd9baaa049")
-combined_nodes.pop(UNUSED_CLERIC_KEY, None)
+UNUSED_CLERIC_KEY: Final[ProgressionsKey] = ("Cleric", "", 1, "2b249feb-bba5-4922-8385-c2dd9baaa049")
+combined_progressions.pop(UNUSED_CLERIC_KEY, None)
 
 # Remove the erroneous Ranger level 12 spell slot
-RANGER_LEVEL_12_KEY: Final[tuple[str, int, str]] = ("Ranger", 12, "0bf247c5-2217-409e-8f88-eee095448f32")
-ranger_level_12_node = combined_nodes[RANGER_LEVEL_12_KEY]
-ranger_level_12_boosts_node = ranger_level_12_node.find("attribute[@id='Boosts'][@value='ActionResource(SpellSlot,1,3)']")
-if ranger_level_12_boosts_node != None:
-    ranger_level_12_node.remove(ranger_level_12_boosts_node)
+if "Ranger" in args.classes:
+    RANGER_LEVEL_12_KEY: Final[ProgressionsKey] = ("Ranger", "", 12, "0bf247c5-2217-409e-8f88-eee095448f32")
+    ranger_level_12_node = combined_progressions[RANGER_LEVEL_12_KEY]
+    ranger_level_12_boosts_node = ranger_level_12_node.find("attribute[@id='Boosts'][@value='ActionResource(SpellSlot,1,3)']")
+    if ranger_level_12_boosts_node != None:
+        ranger_level_12_node.remove(ranger_level_12_boosts_node)
 
-feat_every_n_levels(combined_nodes, args.feats)
-action_resources_multiplier(combined_nodes, args.actions)
+feat_every_n_levels(combined_progressions, args.feats)
+action_resources_multiplier(combined_progressions, args.actions)
 
-sort_node_attributes(combined_nodes)
+sort_node_attributes(combined_progressions)
 
-progressions_name = f"Progressions-{"-".join(sorted([c for c in args.classes]))}-F{args.feats}-A{args.actions}"
+if (args.classes == CLASS_NAMES):
+    progressions_name = f"Progressions-All-F{args.feats}-A{args.actions}"
+else:
+    progressions_name = f"Progressions-{"-".join(sorted([c for c in args.classes]))}-F{args.feats}-A{args.actions}"
 
 mod_base_dir = os.path.join(PARENT_DIR, progressions_name)
 mod_meta_dir = os.path.join(mod_base_dir, "Mods", progressions_name)
@@ -197,7 +259,7 @@ progressions = ElementTree.fromstring("""\
 </save>
 """)
 progressions.find("./region[@id='Progressions']/node[@id='root']/children").extend(
-    [combined_nodes[key] for key in sorted(combined_nodes.keys())])
+    [combined_progressions[key] for key in sorted(combined_progressions.keys())])
 ElementTree.indent(progressions, space=" "*4)
 
 progressions_file = os.path.join(mod_progressions_dir, "Progressions.lsx")

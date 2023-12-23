0db456e162d7687998dd8477fe8839451d17d130
Justin Elliott <justin@elliott.cc>
2023-12-23 13:51:54 -0500

    Go back to basics.

--- Serenade.py
+++ Serenade.py
@@ -27,6 +27,28 @@ loca["Serenade_Description"] = {"en": """
     the lute's melody, truly divine.
     """}
 
+serenade.add_root_templates([
+    Lsx.Node("GameObjects", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["Serenade_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["Serenade_Description"], version="1"),
+        Lsx.Attribute("LevelName", "FixedString", value=""),
+        Lsx.Attribute("MapKey", "FixedString", value="06b0b5a6-4f6c-4ee8-b4e1-6f65866b3ec5"),
+        Lsx.Attribute("Name", "LSString", value="ARM_Instrument_Lute_Serenade"),
+        Lsx.Attribute("ParentTemplateId", "FixedString", value="f2487101-548f-4494-9ec8-b20fa3ad6f7b"),
+        Lsx.Attribute("PhysicsTemplate", "FixedString", value="5d5007e5-cb6f-30ad-3d20-f762ea437673"),
+        Lsx.Attribute("Type", "FixedString", value="item"),
+        Lsx.Attribute("VisualTemplate", "FixedString", value="cfae4ff4-56ac-7bb8-9073-d732ef510c05"),
+        Lsx.Attribute("_OriginalFileVersion_", "int64", value="1"),
+    ])
+])
+
+serenade.set_treasure_table("""\
+new treasuretable "TUT_Chest_Potions"
+CanMerge 1
+new subtable "1,1"
+object category "I_ARM_Instrument_Lute_Serenade",1,0,0,0,0,0,0,0
+""")
+
 loca["Medley_DisplayName"] = {"en": "Medley"}
 loca["Medley_Description"] = {"en": "Perform a medley of songs to inspire and fortify your allies."}
 loca["MEDLEY_Description"] = {"en": """
@@ -41,18 +63,6 @@ loca["MEDLEY_Description"] = {"en": """
 
     You can see in the dark up to [3].
     """}
-
-loca["Virtuoso_DisplayName"] = {"en": "Virtuoso"}
-loca["Virtuoso_Description"] = {"en": """
-    You gain <LSTag Type="Tooltip" Tooltip="Expertise">Expertise</LSTag> in all
-    <LSTag Tooltip="Charisma">Charisma</LSTag> Skills, and have
-    <LSTag Type="Tooltip" Tooltip="ProficiencyBonus">Proficiency</LSTag> in, and
-    <LSTag Tooltip="Advantage">Advantage</LSTag> on, Charisma <LSTag Tooltip="AbilityCheck">Checks</LSTag>.
-    """}
-
-# Regex for data match:
-# data "([^"]+)"\s*("[^"]+")
-# $1=$2,
 
 serenade.add_spell_data(
     "Serenade_Medley",
@@ -82,8 +92,6 @@ serenade.add_spell_data(
     VerbalIntent="Buff",
 )
 
-extra_spellslot_range = range(2, 10, 2)
-
 serenade.add_status_data(
     "SERENADE_MEDLEY",
     StatusType="BOOST",
@@ -105,149 +113,11 @@ serenade.add_status_data(
         "DarkvisionRangeMin(18)",
         "ActiveCharacterLight(c46e7ba8-e746-7020-5146-287474d7b9f7)",
     ],
-    Passives=[
-        *[f"Serenade_Extra_SpellSlots_{extra:02}" for extra in extra_spellslot_range],
-    ],
     TickType="StartTurn",
     TickFunctors="IF(HasHPPercentageLessThan(100) and not IsDowned() and not Dead()):" +
                  "RegainHitPoints(LevelMapValue(Serenade_HealValue))",
     StatusGroups="SG_RemoveOnRespec",
 )
-
-loca["Serenade_Shout_Extra_SpellSlots_DisplayName"] = {"en": "Multiply Spell Slots"}
-loca["Serenade_Shout_Extra_SpellSlots_Description"] = {"en": "Multiply Spell Slots"}
-
-serenade.add_spell_data(
-    "Serenade_Shout_Extra_SpellSlots",
-    SpellType="Shout",
-    ContainerSpells=[
-        "Serenade_Shout_Extra_SpellSlots_00",
-        *[f"Serenade_Shout_Extra_SpellSlots_{extra:02}" for extra in extra_spellslot_range],
-    ],
-    TargetConditions="Self()",
-    DisplayName=loca["Serenade_Shout_Extra_SpellSlots_DisplayName"],
-    Description=loca["Serenade_Shout_Extra_SpellSlots_Description"],
-    Icon="Action_Song_SingForMe",
-    CastTextEvent="Cast",
-    SpellAnimation=[
-        "03496c4a-49e0-4132-b585-3e5ecd1ad8e5,,",
-        ",,",
-        "8252328a-66dd-4dc0-bbe0-00eea3204922,,",
-        "982d842b-5d44-4ef6-ab33-14d5ae514a50,,",
-        "a9682ef9-5d9e-4ac0-8144-2c7fe6eb868c,,",
-        ",,",
-        "32fb4d91-7fde-4b05-9144-ea87b9a4284a,,",
-        "dada6495-752c-4f30-a503-f05b8c811e2b,,",
-        "8ce53f9b-b559-49cd-9607-1991545060d7,,",
-    ],
-    SpellFlags=[
-        "IgnorePreviouslyPickedEntities",
-        "IsLinkedSpellContainer",
-        "Temporary",
-        "UnavailableInDialogs",
-    ],
-    HitAnimationType="MagicalNonDamage",
-    CastSound="Spell_Cast_Buff_EnhanceAbilityBearsEndurance_L1to3",
-    TargetSound="Spell_Impact_Buff_EnhanceAbilityBearsEndurance_L1to3",
-    PrepareEffect="15908bab-2ec3-4abc-a282-c3bf5f2b1387",
-    CastEffect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-    TargetEffect="4d80e719-6b5a-4a77-829c-f9b7f38fd966",
-    UseCosts="",
-    VerbalIntent="Buff",
-)
-
-loca["Serenade_Shout_Extra_SpellSlots_00_DisplayName"] = {"en": "Reset Spell Slots"}
-loca["Serenade_Shout_Extra_SpellSlots_00_Description"] = {
-    "en": """Reset your <LSTag Tooltip="SpellSlot">Spell Slots</LSTag> to the normal number."""
-}
-
-serenade.add_spell_data(
-    "Serenade_Shout_Extra_SpellSlots_00",
-    SpellType="Shout",
-    using="Serenade_Shout_Extra_SpellSlots",
-    DisplayName=loca["Serenade_Shout_Extra_SpellSlots_00_DisplayName"],
-    Description=loca["Serenade_Shout_Extra_SpellSlots_00_Description"],
-    Icon="PassiveFeature_Portent",
-    ContainerSpells="",
-    SpellContainerID="Serenade_Shout_Extra_SpellSlots",
-    SpellProperties=[
-        *[f"RemoveStatus(SERENADE_AURA_EXTRA_SPELLSLOTS_{extra:02})" for extra in extra_spellslot_range],
-    ],
-)
-
-for extra in extra_spellslot_range:
-    additional = extra - 1  # Deduct existing spell slots
-
-    loca[f"Serenade_Shout_Extra_SpellSlots_{extra:02}_DisplayName"] = {"en": f"Multiply Spell Slots by {extra}"}
-    loca[f"Serenade_Shout_Extra_SpellSlots_{extra:02}_Description"] = {
-        "en": f"""Multiply your <LSTag Tooltip="SpellSlot">Spell Slots</LSTag> by {extra}."""
-    }
-
-    serenade.add_spell_data(
-        f"Serenade_Shout_Extra_SpellSlots_{extra:02}",
-        SpellType="Shout",
-        using="Serenade_Shout_Extra_SpellSlots",
-        DisplayName=loca[f"Serenade_Shout_Extra_SpellSlots_{extra:02}_DisplayName"],
-        Description=loca[f"Serenade_Shout_Extra_SpellSlots_{extra:02}_Description"],
-        Icon=f"PassiveFeature_Portent_{extra}",
-        ContainerSpells="",
-        SpellContainerID="Serenade_Shout_Extra_SpellSlots",
-        SpellProperties=f"ApplyStatus(SERENADE_AURA_EXTRA_SPELLSLOTS_{extra:02},100,-1)",
-    )
-
-    serenade.add_passive_data(
-        f"Serenade_Extra_SpellSlots_{extra:02}",
-        Properties="IsHidden",
-        Boosts=[
-            f"ActionResource(SpellSlot,{2*additional},1)",
-            f"IF(CharacterLevelGreaterThan(1)):ActionResource(SpellSlot,{1*additional},1)",
-            f"IF(CharacterLevelGreaterThan(2)):ActionResource(SpellSlot,{1*additional},1)",
-            f"IF(CharacterLevelGreaterThan(2)):ActionResource(SpellSlot,{2*additional},2)",
-            f"IF(CharacterLevelGreaterThan(3)):ActionResource(SpellSlot,{1*additional},2)",
-            f"IF(CharacterLevelGreaterThan(4)):ActionResource(SpellSlot,{2*additional},3)",
-            f"IF(CharacterLevelGreaterThan(5)):ActionResource(SpellSlot,{1*additional},3)",
-            f"IF(CharacterLevelGreaterThan(6)):ActionResource(SpellSlot,{1*additional},4)",
-            f"IF(CharacterLevelGreaterThan(7)):ActionResource(SpellSlot,{1*additional},4)",
-            f"IF(CharacterLevelGreaterThan(8)):ActionResource(SpellSlot,{1*additional},4)",
-            f"IF(CharacterLevelGreaterThan(8)):ActionResource(SpellSlot,{1*additional},5)",
-            f"IF(CharacterLevelGreaterThan(9)):ActionResource(SpellSlot,{1*additional},5)",
-            f"IF(CharacterLevelGreaterThan(10)):ActionResource(SpellSlot,{1*additional},6)",
-        ],
-        BoostConditions=[
-            f"HasStatus('SERENADE_AURA_EXTRA_SPELLSLOTS_{extra:02}') or "
-            f"HasStatus('SERENADE_EXTRA_SPELLSLOTS_{extra:02}')",
-        ],
-    )
-
-    serenade.add_status_data(
-        f"SERENADE_AURA_EXTRA_SPELLSLOTS_{extra:02}",
-        StatusType="BOOST",
-        AuraRadius="36",
-        AuraStatuses=f"IF(Party()):ApplyStatus(SERENADE_EXTRA_SPELLSLOTS_{extra:02})",
-        StackId="SERENADE_AURA_EXTRA_SPELLSLOTS",
-        StackType="Overwrite",
-        StatusPropertyFlags=[
-            "DisableCombatlog",
-            "DisableImmunityOverhead",
-            "DisableOverhead",
-            "DisablePortraitIndicator",
-            "IgnoreResting",
-        ],
-    )
-
-    serenade.add_status_data(
-        f"SERENADE_EXTRA_SPELLSLOTS_{extra:02}",
-        StatusType="BOOST",
-        StackId="SERENADE_EXTRA_SPELLSLOTS",
-        StackType="Overwrite",
-        StatusPropertyFlags=[
-            "DisableCombatlog",
-            "DisableImmunityOverhead",
-            "DisableOverhead",
-            "DisablePortraitIndicator",
-        ],
-        StatusGroups="SG_RemoveOnRespec",
-    )
 
 serenade.add_armor(
     "ARM_Instrument_Lute_Serenade",
@@ -265,6 +135,14 @@ serenade.add_armor(
     ],
     Weight="0.01",
 )
+
+loca["Virtuoso_DisplayName"] = {"en": "Virtuoso"}
+loca["Virtuoso_Description"] = {"en": """
+    You gain <LSTag Type="Tooltip" Tooltip="Expertise">Expertise</LSTag> in all
+    <LSTag Tooltip="Charisma">Charisma</LSTag> Skills, and have
+    <LSTag Type="Tooltip" Tooltip="ProficiencyBonus">Proficiency</LSTag> in, and
+    <LSTag Tooltip="Advantage">Advantage</LSTag> on, Charisma <LSTag Tooltip="AbilityCheck">Checks</LSTag>.
+    """}
 
 serenade.add_passive_data(
     "Serenade_Virtuoso",
@@ -287,177 +165,6 @@ serenade.add_passive_data(
     ],
 )
 
-
-def spell_boost_ability(ability: str, icon: str, cast_sound: str, target_sound: str, prepare_effect: str,
-                        cast_effect: str, target_effect: str, boost_range: range = range(2, 22, 2)) -> None:
-    loca[f"Serenade_Boost_{ability}_DisplayName"] = {"en": f"Serenade: Boost {ability}"}
-    loca[f"Serenade_Boost_{ability}_Description"] = {
-        "en": f"""Increase your <LSTag Tooltip="{ability}">{ability}</LSTag> by a selected amount."""
-    }
-    loca[f"Serenade_Boost_{ability}_00_DisplayName"] = {"en": f"Reset {ability}"}
-    loca[f"Serenade_Boost_{ability}_00_Description"] = {
-        "en": f"""Reset your <LSTag Tooltip="{ability}">{ability}</LSTag> to its unboosted value."""
-    }
-
-    serenade.add_spell_data(
-        f"Serenade_Boost_{ability}",
-        SpellType="Shout",
-        ContainerSpells=[
-            f"Serenade_Boost_{ability}_00",
-            *[f"Serenade_Boost_{ability}_{boost:02}" for boost in boost_range]
-        ],
-        CycleConditions="Self()",
-        TargetConditions="Self()",
-        DisplayName=loca[f"Serenade_Boost_{ability}_DisplayName"],
-        Description=loca[f"Serenade_Boost_{ability}_Description"],
-        Icon=icon,
-        CastTextEvent="Cast",
-        SpellAnimation=[
-            "03496c4a-49e0-4132-b585-3e5ecd1ad8e5,,",
-            ",,",
-            "8252328a-66dd-4dc0-bbe0-00eea3204922,,",
-            "982d842b-5d44-4ef6-ab33-14d5ae514a50,,",
-            "a9682ef9-5d9e-4ac0-8144-2c7fe6eb868c,,",
-            ",,",
-            "32fb4d91-7fde-4b05-9144-ea87b9a4284a,,",
-            "dada6495-752c-4f30-a503-f05b8c811e2b,,",
-            "8ce53f9b-b559-49cd-9607-1991545060d7,,",
-        ],
-        SpellFlags=[
-            "IgnorePreviouslyPickedEntities",
-            "IsLinkedSpellContainer",
-            "Temporary",
-            "UnavailableInDialogs",
-        ],
-        HitAnimationType="MagicalNonDamage",
-        CastSound=cast_sound,
-        TargetSound=target_sound,
-        PrepareEffect=prepare_effect,
-        CastEffect=cast_effect,
-        TargetEffect=target_effect,
-        UseCosts="",
-        VerbalIntent="Buff",
-    )
-
-    serenade.add_spell_data(
-        f"Serenade_Boost_{ability}_00",
-        SpellType="Shout",
-        using=f"Serenade_Boost_{ability}",
-        DisplayName=loca[f"Serenade_Boost_{ability}_00_DisplayName"],
-        Description=loca[f"Serenade_Boost_{ability}_00_Description"],
-        Icon="PassiveFeature_Portent",
-        ContainerSpells="",
-        SpellContainerID=f"Serenade_Boost_{ability}",
-        SpellProperties=f"RemoveStatus(SERENADE_BOOST_{ability.upper()})",
-    )
-
-    for boost in boost_range:
-        loca[f"Serenade_Boost_{ability}_{boost:02}_DisplayName"] = {"en": f"Boost {ability} by {boost}"}
-        loca[f"Serenade_Boost_{ability}_{boost:02}_Description"] = {
-            "en": f"""Increase your <LSTag Tooltip="{ability}">{ability}</LSTag> by {boost}."""
-        }
-
-        serenade.add_spell_data(
-            f"Serenade_Boost_{ability}_{boost:02}",
-            SpellType="Shout",
-            using=f"Serenade_Boost_{ability}",
-            DisplayName=loca[f"Serenade_Boost_{ability}_{boost:02}_DisplayName"],
-            Description=loca[f"Serenade_Boost_{ability}_{boost:02}_Description"],
-            Icon=f"PassiveFeature_Portent_{boost}",
-            ContainerSpells="",
-            SpellContainerID=f"Serenade_Boost_{ability}",
-            SpellProperties=f"ApplyStatus(SERENADE_BOOST_{ability.upper()},100,{boost})",
-        )
-
-    serenade.add_status_data(
-        f"SERENADE_BOOST_{ability.upper()}",
-        StatusType="BOOST",
-        DisplayName=loca[f"Serenade_Boost_{ability}_DisplayName"],
-        Description=loca[f"Serenade_Boost_{ability}_Description"],
-        Icon=icon,
-        StackId=f"SERENADE_BOOST_{ability.upper()}",
-        StackType="Overwrite",
-        Boosts=f"Ability({ability},1,30)",
-        StatusPropertyFlags=[
-            "DisableCombatlog",
-            "DisableImmunityOverhead",
-            "DisableOverhead",
-            "DisablePortraitIndicator",
-            "FreezeDuration",
-            "IgnoreResting",
-            "MultiplyEffectsByDuration",
-        ],
-    )
-
-
-spell_boost_ability("Strength",
-                    icon="Spell_Transmutation_EnhanceAbility_BullsStrenght",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityBullsStrength_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityBullsStrength_L1to3",
-                    prepare_effect="5ea8f8f4-ba5f-4417-82f6-ed2ce4ffe264",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="fbb955f8-a644-451b-89bd-7950ad4cebad")
-spell_boost_ability("Dexterity",
-                    icon="Spell_Transmutation_EnhanceAbility_CatsGrace",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityCatsGrace_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityCatsGrace_L1to3",
-                    prepare_effect="fbce561c-fd42-4626-bf04-8461f46dfbc8",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="474d55bf-bce6-401b-872a-1922c8d54d99")
-spell_boost_ability("Constitution",
-                    icon="Spell_Transmutation_EnhanceAbility_BearsEndurance",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityBearsEndurance_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityBearsEndurance_L1to3",
-                    prepare_effect="15908bab-2ec3-4abc-a282-c3bf5f2b1387",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="4d80e719-6b5a-4a77-829c-f9b7f38fd966")
-spell_boost_ability("Intelligence",
-                    icon="Spell_Transmutation_EnhanceAbility_FoxsCunning",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityFoxsCunning_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityFoxsCunning_L1to3",
-                    prepare_effect="1ee00587-5c1a-4068-aba3-6bfd5cb8f92f",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="587df9a6-10c6-4125-ab0a-73c477018a4b")
-spell_boost_ability("Wisdom",
-                    icon="Spell_Transmutation_EnhanceAbility_OwlsWisdom",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityOwlsWisdom_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityOwlsWisdom_L1to3",
-                    prepare_effect="1082b19d-920d-423f-b787-3c66da153f47",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="b01d8d96-abb3-4e88-8e41-ce12c7dbf30a")
-spell_boost_ability("Charisma",
-                    icon="Spell_Transmutation_EnhanceAbility_EaglesSplendor",
-                    cast_sound="Spell_Cast_Buff_EnhanceAbilityEaglesSplendor_L1to3",
-                    target_sound="Spell_Impact_Buff_EnhanceAbilityEaglesSplendor_L1to3",
-                    prepare_effect="fa18f4ad-7f12-47fc-9fe7-3a157e0ee260",
-                    cast_effect="bcd66fb0-b0bc-41d0-abba-ad443d63dd72",
-                    target_effect="70d8d0dc-e4ff-42ed-8503-09bbf2fbbeda")
-
-loca["Serenade_Retune_DisplayName"] = {"en": "Serenade: Retune"}
-loca["Serenade_Retune_Description"] = {"en": "Unlock spells to retune your abilities."}
-
-serenade.add_passive_data(
-    "Serenade_Retune",
-    DisplayName=loca["Serenade_Retune_DisplayName"],
-    Description=loca["Serenade_Retune_Description"],
-    Icon="Action_Perform_Lute",
-    Properties=[
-        "IsToggled",
-        "ToggledDefaultAddToHotbar",
-        "ToggleForParty",
-    ],
-    Boosts=[
-        "UnlockSpell(Serenade_Boost_Strength)",
-        "UnlockSpell(Serenade_Boost_Dexterity)",
-        "UnlockSpell(Serenade_Boost_Constitution)",
-        "UnlockSpell(Serenade_Boost_Intelligence)",
-        "UnlockSpell(Serenade_Boost_Wisdom)",
-        "UnlockSpell(Serenade_Boost_Charisma)",
-        "UnlockSpell(Serenade_Shout_Extra_SpellSlots)",
-    ],
-    ToggleGroup="Serenade_Retune",
-)
-
 serenade.add_level_maps([
     Lsx.Node("LevelMapSeries", [
         *[Lsx.Attribute(f"Level{level}", "LSString", value=f"{int(level * 2.5)}") for level in range(1, 21)],
@@ -471,27 +178,5 @@ serenade.add_level_maps([
         Lsx.Attribute("UUID", "guid", value="803d7210-940d-4692-8178-436e3d711818"),
     ]),
 ])
-
-serenade.add_root_templates([
-    Lsx.Node("GameObjects", [
-        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["Serenade_DisplayName"], version="1"),
-        Lsx.Attribute("Description", "TranslatedString", handle=loca["Serenade_Description"], version="1"),
-        Lsx.Attribute("LevelName", "FixedString", value=""),
-        Lsx.Attribute("MapKey", "FixedString", value="06b0b5a6-4f6c-4ee8-b4e1-6f65866b3ec5"),
-        Lsx.Attribute("Name", "LSString", value="ARM_Instrument_Lute_Serenade"),
-        Lsx.Attribute("ParentTemplateId", "FixedString", value="f2487101-548f-4494-9ec8-b20fa3ad6f7b"),
-        Lsx.Attribute("PhysicsTemplate", "FixedString", value="5d5007e5-cb6f-30ad-3d20-f762ea437673"),
-        Lsx.Attribute("Type", "FixedString", value="item"),
-        Lsx.Attribute("VisualTemplate", "FixedString", value="cfae4ff4-56ac-7bb8-9073-d732ef510c05"),
-        Lsx.Attribute("_OriginalFileVersion_", "int64", value="1"),
-    ])
-])
-
-serenade.set_treasure_table("""\
-new treasuretable "TUT_Chest_Potions"
-CanMerge 1
-new subtable "1,1"
-object category "I_ARM_Instrument_Lute_Serenade",1,0,0,0,0,0,0,0
-""")
 
 serenade.build()

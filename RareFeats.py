33733207531c582c006ce81bad998c47bbf2beea
Justin Elliott <justin@elliott.cc>
2024-01-20 22:52:03 -0500

    Add feats without ASI.

--- RareFeats.py
+++ RareFeats.py
@@ -77,4 +77,165 @@ for asi in [4, 8, 16, 32, 44]:
         ]),
     ])
 
+# Athlete without the ASI
+athlete_uuid = rare_feats.make_uuid("RareFeats_Athlete")
+
+loca["RareFeats_Athlete_DisplayName"] = {"en": "Rare Feats: Athlete"}
+loca["RareFeats_Athlete_Description"] = {"en": """
+    When you are Prone, standing up uses significantly less movement.
+    Your <LSTag Type="Spell" Tooltip="Projectile_Jump">Jump</LSTag> distance also increases by 50%.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_Athlete_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_Athlete_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_Athlete"),
+        Lsx.Attribute("FeatId", "guid", value=str(athlete_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_Athlete"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_Athlete"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="Athlete_StandUp"),
+        Lsx.Attribute("UUID", "guid", value=str(athlete_uuid)),
+    ]),
+])
+
+# Lightly Armored without the ASI
+lightly_armored_uuid = rare_feats.make_uuid("RareFeats_LightlyArmored")
+
+loca["RareFeats_LightlyArmored_DisplayName"] = {"en": "Rare Feats: Lightly Armoured"}
+loca["RareFeats_LightlyArmored_Description"] = {"en": """
+    You gain <LSTag Tooltip="ArmourProficiency">Armour Proficiency</LSTag> with Light Armour.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_LightlyArmored_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_LightlyArmored_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_LightlyArmored"),
+        Lsx.Attribute("FeatId", "guid", value=str(lightly_armored_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_LightlyArmored"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_LightlyArmored"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="LightlyArmored"),
+        Lsx.Attribute("UUID", "guid", value=str(lightly_armored_uuid)),
+    ]),
+])
+
+# ModeratelyArmored without the ASI
+moderately_armored_uuid = rare_feats.make_uuid("RareFeats_ModeratelyArmored")
+
+loca["RareFeats_ModeratelyArmored_DisplayName"] = {"en": "Rare Feats: Moderately Armoured"}
+loca["RareFeats_ModeratelyArmored_Description"] = {"en": """
+    You gain <LSTag Tooltip="ArmourProficiency">Armour Proficiency</LSTag> with Medium Armour and shields.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_ModeratelyArmored_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_ModeratelyArmored_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_ModeratelyArmored"),
+        Lsx.Attribute("FeatId", "guid", value=str(moderately_armored_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_ModeratelyArmored"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_ModeratelyArmored"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="ModeratelyArmored"),
+        Lsx.Attribute("Requirements", "LSString", value="FeatRequirementProficiency('LightArmor')"),
+        Lsx.Attribute("UUID", "guid", value=str(moderately_armored_uuid)),
+    ]),
+])
+
+# Performer without the ASI
+performer_uuid = rare_feats.make_uuid("RareFeats_Performer")
+
+loca["RareFeats_Performer_DisplayName"] = {"en": "Rare Feats: Performer"}
+loca["RareFeats_Performer_Description"] = {"en": """
+    You gain <LSTag Tooltip="MusicalInstrumentProficiency">Musical Instrument Proficiency</LSTag>.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_Performer_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_Performer_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_Performer"),
+        Lsx.Attribute("FeatId", "guid", value=str(performer_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_Performer"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_Performer"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="Performer"),
+        Lsx.Attribute("UUID", "guid", value=str(performer_uuid)),
+    ]),
+])
+
+# TavernBrawler without the ASI
+tavern_brawler_uuid = rare_feats.make_uuid("RareFeats_TavernBrawler")
+
+loca["RareFeats_TavernBrawler_DisplayName"] = {"en": "Rare Feats: Tavern Brawler"}
+loca["RareFeats_TavernBrawler_Description"] = {"en": """
+    When you make an unarmed attack, use an improvised weapon, or throw something, your Strength
+    <LSTag Tooltip="AbilityModifier">Modifier</LSTag> is added twice to the damage and
+    <LSTag Tooltip="AttackRoll">Attack Rolls</LSTag>.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_TavernBrawler_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_TavernBrawler_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_TavernBrawler"),
+        Lsx.Attribute("FeatId", "guid", value=str(tavern_brawler_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_TavernBrawler"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_TavernBrawler"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="TavernBrawler"),
+        Lsx.Attribute("UUID", "guid", value=str(tavern_brawler_uuid)),
+    ]),
+])
+
+# WeaponMaster without the ASI
+weapon_master_uuid = rare_feats.make_uuid("RareFeats_WeaponMaster")
+
+loca["RareFeats_WeaponMaster_DisplayName"] = {"en": "Rare Feats: Weapon Master"}
+loca["RareFeats_WeaponMaster_Description"] = {"en": """
+    You gain <LSTag Tooltip="Proficiency">Proficiency</LSTag> with four weapons of your choice.
+    """}
+
+rare_feats.add_feat_descriptions([
+    Lsx.Node("FeatDescription", [
+        Lsx.Attribute("DisplayName", "TranslatedString", handle=loca["RareFeats_WeaponMaster_DisplayName"], version="1"),
+        Lsx.Attribute("Description", "TranslatedString", handle=loca["RareFeats_WeaponMaster_Description"], version="1"),
+        Lsx.Attribute("ExactMatch", "FixedString", value="RareFeats_WeaponMaster"),
+        Lsx.Attribute("FeatId", "guid", value=str(weapon_master_uuid)),
+        Lsx.Attribute("UUID", "guid", value=str(rare_feats.make_uuid("RareFeats_FeatDescription_WeaponMaster"))),
+    ]),
+])
+
+rare_feats.add_feats([
+    Lsx.Node("Feat", [
+        Lsx.Attribute("Name", "FixedString", value="RareFeats_WeaponMaster"),
+        Lsx.Attribute("PassivesAdded", "LSString", value="WeaponMaster"),
+        Lsx.Attribute("Selectors", "LSString", value="SelectPassives(f21e6b94-44e8-4ae0-a6f1-0c81abac03a2,4,WeaponMasterProficiencies)"),
+        Lsx.Attribute("UUID", "guid", value=str(weapon_master_uuid)),
+    ]),
+])
+
 rare_feats.build()

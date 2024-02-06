c6d452e77140680b8c979e85c4cf9c3a1d3dd4c4
Justin Elliott <justin@elliott.cc>
2024-02-06 14:11:24 -0500

    Added Daisy, ItemTweaks.

---
+++ Daisy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Generates files for the "Daisy" mod.
+"""
+
+import os
+
+from modtools.gamedata import armor_data
+from modtools.mod import Mod
+from uuid import UUID
+
+daisy = Mod(os.path.dirname(__file__),
+            author="justin-elliott",
+            name="Daisy",
+            mod_uuid=UUID("fc71b9c6-905c-40fe-8c16-5ad8345dfaa4"),
+            description="Adds the Guardian armor to the tutorial chest.")
+
+daisy.add(armor_data(
+    "Daisy_Clothing_Armor",
+    using="ARM_Robe_Body",
+    Rarity="Legendary",
+    RootTemplate="aa0917ea-5f66-4a22-97de-654228484128",
+))
+
+daisy.add(armor_data(
+    "Daisy_Clothing_Gloves",
+    using="_Hand",
+    Rarity="Legendary",
+    RootTemplate="5a0ee632-9145-48b2-9b92-97c32c2ccbd9",
+))
+
+daisy.add(armor_data(
+    "Daisy_Clothing_Boots",
+    using="_Foot",
+    Rarity="Legendary",
+    RootTemplate="216f0362-f77b-420c-84cb-d84853aa173d",
+))
+
+daisy.add_treasure_table("""\
+new treasuretable "TUT_Chest_Potions"
+CanMerge 1
+new subtable "1,1"
+object category "I_Daisy_Clothing_Armor",1,0,0,0,0,0,0,0
+new subtable "1,1"
+object category "I_Daisy_Clothing_Gloves",1,0,0,0,0,0,0,0
+new subtable "1,1"
+object category "I_Daisy_Clothing_Boots",1,0,0,0,0,0,0,0
+""")
+
+daisy.build()
---
+++ ItemTweaks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Generates files for the "ItemTweaks" mod.
+"""
+
+import os
+
+from modtools.gamedata import armor_data
+from modtools.mod import Mod
+from uuid import UUID
+
+item_tweaks = Mod(os.path.dirname(__file__),
+                  author="justin-elliott",
+                  name="ItemTweaks",
+                  mod_uuid=UUID("0e8ef3be-8daa-4c7f-a41d-3a72ae00c33d"),
+                  description="Tweaks various game items.")
+
+# The Smuggler's Ring now provides advantage on dexterity checks, and boosts all dexterity-based skills.
+item_tweaks.add(armor_data(
+    "PLA_SmugglerRing",
+    using="PLA_SmugglerRing",
+    Boosts=[
+        "Skill(Acrobatics,2)"
+        "Skill(SleightOfHand,2)",
+        "Skill(Stealth,2)",
+        "Advantage(Ability,Dexterity)",
+    ],
+))
+
+item_tweaks.build()

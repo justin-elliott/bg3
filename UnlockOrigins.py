d0990bbf8087f8b118c2cc2e012dac85437388ba
Justin Elliott <justin@elliott.cc>
2024-04-23 13:50:13 -0400

    Remove race unlock.

--- UnlockOrigins.py
+++ UnlockOrigins.py
@@ -22,13 +22,12 @@ class UnlockOrigins(Replacer):
     def unlock_origins(self, origin: Origin) -> None:
         origin.BackgroundUUID = None
         origin.LockClass = None
-        origin.LockRace = None
 
     def __init__(self):
         super().__init__(os.path.dirname(__file__),
                          author="justin-elliott",
                          name="UnlockOrigins",
-                         description="Unlocks background, class, and race for origin characters.")
+                         description="Unlocks background and class for origin characters.")
 
 
 def main():

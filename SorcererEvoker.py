cd9627547676504f12f335585dddc9151bb9ecf9
Justin Elliott <justin@elliott.cc>
2024-07-31 18:35:38 -0400

    Adjust advancement.

--- SorcererEvoker.py
+++ SorcererEvoker.py
@@ -52,20 +52,7 @@ class SorcererEvoker(Replacer):
         spell_list = str(self.make_uuid("level_1_spell_list"))
         self.mod.add(SpellList(
             Comment="SorcererEvoker level 1 spells",
-            Spells=[
-                self._bolster,
-                "Projectile_EldritchBlast",
-            ],
-            UUID=spell_list,
-        ))
-        return spell_list
-
-    @cached_property
-    def _level_1_storm_sorcery_spell_list(self) -> str:
-        spell_list = str(self.make_uuid("level_1_storm_sorcery_spell_list"))
-        self.mod.add(SpellList(
-            Comment="SorcererEvoker level 1 Storm Sorcery spells",
-            Spells=["Target_ShockingGrasp", "Projectile_WitchBolt"],
+            Spells=[self._bolster],
             UUID=spell_list,
         ))
         return spell_list
@@ -131,15 +118,10 @@ class SorcererEvoker(Replacer):
 
     @progression(CharacterClass.SORCERER, 1)
     def level_1(self, progression: Progression) -> None:
-        selectors = [
-            selector for selector in progression.Selectors
-            if not selector.startswith("SelectSkills") and not selector.startswith("SelectSpells")
-        ]
+        selectors = [selector for selector in progression.Selectors if not selector.startswith("SelectSkills")]
         selectors.extend([
             "SelectSkills(f974ebd6-3725-4b90-bb5c-2b647d41615d,5)",
             "SelectSkillsExpertise(f974ebd6-3725-4b90-bb5c-2b647d41615d,2)",
-            "SelectSpells(485a68b4-c678-4888-be63-4a702efbe391,5,0,SorcererCantrip,,,AlwaysPrepared)",
-            "SelectSpells(92c4751f-6255-4f67-822c-a75d53830b27,2,0,SorcererSpell)",
         ])
         progression.Selectors = selectors
 
@@ -149,30 +131,23 @@ class SorcererEvoker(Replacer):
         progression.PassivesAdded += [
             self._fast_movement_30,
             self._pack_mule,
-            "SculptSpells",
             self._warding,
         ]
         progression.Selectors += [
             f"AddSpells({self._level_1_spell_list},,,,AlwaysPrepared)"
         ]
 
-    @progression(CharacterClass.SORCERER_STORM, 1)
-    def level_1_storm_sorcery(self, progression: Progression) -> None:
-        progression.Selectors = (progression.Selectors or []) + [
-            f"AddSpells({self._level_1_storm_sorcery_spell_list},,,,AlwaysPrepared)"
-        ]
-
     @progression(CharacterClass.SORCERER, 2)
     def level_2(self, progression: Progression) -> None:
-        progression.PassivesAdded = (progression.PassivesAdded or []) + ["Blindsight", "SuperiorDarkvision"]
+        progression.PassivesAdded = (progression.PassivesAdded or []) + ["DevilsSight", "SculptSpells"]
 
     @progression(CharacterClass.SORCERER, 3)
     def level_3(self, progression: Progression) -> None:
-        progression.PassivesAdded = (progression.PassivesAdded or []) + ["AgonizingBlast", "RepellingBlast"]
+        progression.PassivesAdded = (progression.PassivesAdded or []) + ["JackOfAllTrades"]
 
     @progression(CharacterClass.SORCERER, 4)
     def level_4(self, progression: Progression) -> None:
-        progression.PassivesAdded = (progression.PassivesAdded or []) + ["JackOfAllTrades"]
+        progression.PassivesAdded = (progression.PassivesAdded or []) + ["ImprovedCritical"]
 
     @progression(CharacterClass.SORCERER, 5)
     def level_5(self, progression: Progression) -> None:
@@ -190,7 +165,7 @@ class SorcererEvoker(Replacer):
 
     @progression(CharacterClass.SORCERER, 8)
     def level_8(self, progression: Progression) -> None:
-        progression.PassivesAdded = (progression.PassivesAdded or []) + ["ImprovedCritical"]
+        progression.PassivesAdded = (progression.PassivesAdded or []) + ["Evasion"]
 
     @progression(CharacterClass.SORCERER, 9)
     def level_9(self, progression: Progression) -> None:
@@ -210,9 +185,6 @@ class SorcererEvoker(Replacer):
     @progression(CharacterClass.SORCERER, 12)
     def level_12(self, progression: Progression) -> None:
         progression.PassivesAdded = (progression.PassivesAdded or []) + ["ReliableTalent"]
-        progression.Selectors = (progression.Selectors or []) + [
-            "AddSpells(964e765d-5881-463e-b1b0-4fc6b8035aa8,,,,AlwaysPrepared)",  # Action Surge
-        ]
 
 
 if __name__ == "__main__":

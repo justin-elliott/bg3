a05a0206b62a64360776d3ab35574a6ce7878971
Justin Elliott <justin@elliott.cc>
2025-06-25 20:37:38 -0400

    Added Huntress.

--- ModMaker.py
+++ ModMaker.py
@@ -7,14 +7,8 @@ import os
 import re
 import textwrap
 
-from modtools.lsx.game import (
-    BASE_CHARACTER_CLASSES,
-    CharacterClass,
-)
-from modtools.lsx.game import Progression
+from modtools.lsx.game import CharacterClass
 from modtools.replacers import (
-    load_progressions,
-    progression,
     Replacer,
 )
 from typing import Final, TextIO
@@ -44,9 +38,7 @@ EPILOGUE = """
 
 def main() -> None:
     {title_snake} = {title}(
-        classes=[
-            {classes}
-        ],
+        classes=[{classes}],
         feats={feats},
         spells={spells},
         warlock_spells={warlock_spells},
@@ -62,22 +54,15 @@ if __name__ == "__main__":
     main()
 """
 
-def filter_classes(progressions: list[Progression], args: Replacer.Args) -> list[Progression]:
-    return [progression for progression in progressions
-            if progression.Name in CharacterClass
-            and CharacterClass(progression.Name) in args.classes
-            and not progression.IsMulticlass]
-
 INDENT: Final[int] = 4
 MAX_LIST_LENGTH: Final[int] = 80
 
-def write_progression(f: TextIO, progress: Progression) -> None:
+def write_progression(f: TextIO, character_class: str, level: int) -> None:
     indent = " " * INDENT
-    class_name = CharacterClass(progress.Name).name
     progression_text = textwrap.dedent(f"""\
 
-        @progression(CharacterClass.{class_name}, {progress.Level})
-        def {class_name.lower()}_level_{progress.Level}(self, _: Progression) -> None:
+        @progression(CharacterClass.{character_class.name}, {level})
+        def {character_class.lower()}_level_{level}(self, _: Progression) -> None:
             raise DontIncludeProgression()
         """)
     f.write(textwrap.indent(progression_text, indent))
@@ -86,9 +71,7 @@ def main() -> None:
     replacer = Replacer(os.path.dirname(__file__),
                         author="justin-elliott",
                         description="A mod maker.")
-    progressions: list[Progression] = load_progressions(replacer.mod)
-    progressions = filter_classes(progressions, replacer.args)
-    
+
     title = re.sub(r"\s", "", replacer.args.name)
     title_snake = re.sub(r"(?<!^)(?=[A-Z])", "_", title).lower()
 
@@ -96,8 +79,9 @@ def main() -> None:
     with open(mod_file, "w") as f:
         f.write(PROLOGUE.format(title=title, classes=", ".join([cls.value for cls in replacer.args.classes])))
 
-        for progress in progressions:
-            write_progression(f, progress)
+        for character_class in replacer.args.classes:
+            for level in range(1, 21):
+                write_progression(f, character_class, level)
         
         classes = f",\n{' ' * INDENT * 3}".join([f"CharacterClass.{cls.name}" for cls in replacer.args.classes])
         feats = ", ".join(map(str, sorted(replacer.args.feats)))

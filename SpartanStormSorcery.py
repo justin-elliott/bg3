3925407348709578ab08c1d19b4190b080110f29
Justin Elliott <justin@elliott.cc>
2025-11-28 20:14:05 -0500

    Add a chest for unworn armor.

--- SpartanStormSorcery.py
+++ SpartanStormSorcery.py
@@ -2,16 +2,16 @@
 import os
 
 from functools import cached_property
-from moddb import character_level_range, EmpoweredSpells
-from modtools.gamedata import Armor, PassiveData, SpellData, StatusData, Weapon
-from modtools.lsx.game import CharacterAbility, Dependencies, Progression, SpellList
+from moddb import character_level_range, Defense, EmpoweredSpells
+from modtools.gamedata import Armor, ObjectData, PassiveData, SpellData, StatusData, Weapon
+from modtools.lsx.game import CharacterAbility, Dependencies, GameObjects, Progression, ProgressionDescription, SpellList
 from modtools.replacers import (
     CharacterClass,
     DontIncludeProgression,
     progression,
     Replacer,
 )
-from modtools.text import Equipment
+from modtools.text import Equipment, TreasureTable
 
 
 class SpartanStormSorcery(Replacer):
@@ -33,6 +33,16 @@ class SpartanStormSorcery(Replacer):
 
         self.mod.add(character_level_range)
         self._add_starting_equipment()
+        self._add_treasure_table()
+
+
+    @cached_property
+    def _empowered_spells(self) -> str:
+        return EmpoweredSpells(self.mod).add_empowered_spells(CharacterAbility.CHARISMA)
+
+    @cached_property
+    def _unarmored_defense(self) -> str:
+        return Defense(self.mod).add_unarmored_defense(CharacterAbility.CHARISMA)
 
     def _weapon_boosts(self, damage_type: str, die_sides: int) -> list[str]:
         return [
@@ -55,7 +65,7 @@ class SpartanStormSorcery(Replacer):
             name,
             using="ARM_SPR_Body",
             PassivesOnEquip=[
-                "SPR_Unarmored",
+                self._unarmored_defense,
             ],
         ))
         return name
@@ -71,7 +81,7 @@ class SpartanStormSorcery(Replacer):
 
     @cached_property
     def _armor_cloak(self) -> str:
-        name = f"{self.mod.get_prefix()}_Boots"
+        name = f"{self.mod.get_prefix()}_Cloak"
         self.mod.add(Armor(
             name,
             using="ARM_SPR_Cloak",
@@ -93,6 +103,8 @@ class SpartanStormSorcery(Replacer):
         self.mod.add(Armor(
             name,
             using="ARM_SPR_Headwear",
+            ArmorType="Cloth",
+            Proficiency_Group="",
         ))
         return name
 
@@ -193,8 +205,6 @@ class SpartanStormSorcery(Replacer):
             add equipmentgroup
             add equipment entry "{self._armor_gloves}"
             add equipmentgroup
-            add equipment entry "{self._armor_helm}"
-            add equipmentgroup
             add equipment entry "{self._spear_instrument}"
             add equipmentgroup
             add equipment entry "OBJ_Scroll_Revivify"
@@ -207,27 +217,98 @@ class SpartanStormSorcery(Replacer):
             add equipmentgroup
             add equipment entry "OBJ_Bag_AlchemyPouch"
             add equipmentgroup
-            add equipment entry "{self._camp_body}"
-            add equipmentgroup
             add equipment entry "{self._camp_body_cloak}"
             add equipmentgroup
             add equipment entry "{self._camp_boots}"
             add equipmentgroup
-            add equipment entry "{self._camp_helm}"
-            add equipmentgroup
             add equipment entry "OBJ_Backpack_CampSupplies"
         """))
+    
+    def _add_container(self, name: str, *, display_name: str, description: str) -> None:
+        container_uuid = self.make_uuid(name)
 
-    @cached_property
-    def _empowered_spells(self) -> str:
-        return EmpoweredSpells(self.mod).add_empowered_spells(CharacterAbility.CHARISMA)
+        loca = self.mod.get_localization()
+        loca[f"{name}_DisplayName"] = {"en": display_name}
+        loca[f"{name}_Description"] = {"en": description}
+
+        self.mod.add(GameObjects(
+            DisplayName=loca[f"{name}_DisplayName"],
+            Description=loca[f"{name}_Description"],
+            Icon="Item_CONT_GEN_Chest_Travel_A_Small_A",
+            LevelName="",
+            MapKey=container_uuid,
+            Name=name,
+            ParentTemplateId="47805d79-88f1-4933-86eb-f78f67cbc33f",
+            Stats=name,
+            Type="item",
+            children=[
+                GameObjects.InventoryList(children=[
+                    GameObjects.InventoryList.InventoryItem(
+                        Object=f"{name}_TreasureTable",
+                    ),
+                ]),
+            ],
+        ))
+
+        self.mod.add(ObjectData(
+            name,
+            using="_Container",
+            RootTemplate=container_uuid,
+            Weight=0.01,
+        ))
+
+    def _add_treasure_table(self) -> None:
+        container_name = f"{self.mod.get_prefix()}_Chest"
+        self._add_container(container_name,
+                            display_name="Storm Sorcerer's Spartan Gear",
+                            description="""
+                                Forged from blackened steel and reinforced with storm-weathered oak, this heavy chest
+                                bears the mark of a Spartan Storm Sorcerer. Its lid is secured by robust iron clasps,
+                                etched with subtle, angular lightning bolts that hint at the raw power contained within.
+                            """)
+
+        self.mod.add(TreasureTable(f"""
+            new treasuretable "TUT_Chest_Potions"
+            CanMerge 1
+            new subtable "1,1"
+            object category "I_{container_name}",1,0,0,0,0,0,0,0
+        """))
+
+        self.mod.add(TreasureTable(f"""
+            new treasuretable "{container_name}_TreasureTable"
+            CanMerge 1
+            new subtable "1,1"
+            object category "I_{self._armor_helm}",1,0,0,0,0,0,0,0
+            new subtable "1,1"
+            object category "I_{self._camp_body}",1,0,0,0,0,0,0,0
+            new subtable "1,1"
+            object category "I_{self._camp_helm}",1,0,0,0,0,0,0,0
+        """))
 
     @progression(CharacterClass.SORCERER_STORM, 1)
     def spartan_level_1(self, progress: Progression) -> None:
+        hp_boost = "IncreaseMaxHP(ClassLevel(Sorcerer)*2)"
+
+        progress.Boosts = [hp_boost]
         progress.PassivesAdded = ["DevilsSight"]
         progress.Selectors = [
             "AddSpells(12150e11-267a-4ecc-a3cc-292c9e2a198d)",  # Fly
         ]
+
+        hp_boost_name = f"{self.mod.get_prefix()}_HPBoost"
+        loca = self.mod.get_localization()
+        loca[f"{hp_boost_name}_DisplayName"] = {"en": "Storm Sorcery: Hit Points"}
+        loca[f"{hp_boost_name}_Description"] = {"en": """
+            Your <LSTag Tooltip="HitPoints">hit point</LSTag> maximum increases by 2 for each Sorcerer level.
+        """}
+
+        self.mod.add(ProgressionDescription(
+            DisplayName=loca[f"{hp_boost_name}_DisplayName"],
+            Description=loca[f"{hp_boost_name}_Description"],
+            ExactMatch=hp_boost,
+            ProgressionId=progress.UUID,
+            UUID=self.make_uuid(hp_boost),
+        ))
 
     @progression(CharacterClass.SORCERER_STORM, 2)
     def spartan_level_2(self, progress: Progression) -> None:

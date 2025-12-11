6c902a1cca11ab9051ed8121d15fd781fdeeb229
Justin Elliott <justin@elliott.cc>
2025-12-10 20:27:36 -0500

    Added BestOfBothWorlds.

---
+++ BestOfBothWorlds.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Generates files for the Best of Both Worlds, a mod to give Half-Elves the features of both of their parent races.
+"""
+
+import os
+
+from modtools.lsx.game import (
+    CharacterClass,
+    CharacterRace,
+    Progression,
+)
+from modtools.replacers import (
+    progression,
+    Replacer,
+)
+
+
+class BestOfBothWorlds(Replacer):
+    def __init__(self, **kwds: str):
+        super().__init__(os.path.dirname(__file__),
+                         author="justin-elliott",
+                         name="BestOfBothWorlds",
+                         description="Enhancements for the Half-Elf subraces.",
+                         **kwds)
+
+    @progression(CharacterRace.HALF_ELF, 1)
+    def half_elf(self, progress: Progression) -> None:
+        progress.Boosts += ["ProficiencyBonus(Skill,Perception)"]
+        progress.PassivesAdded += ["Elf_WeaponTraining"]
+
+
+if __name__ == "__main__":
+    best_of_both_worlds = BestOfBothWorlds(
+        classes=[CharacterClass.ROGUE],  # Ignored, but prevents multiclass slots from being updated
+    )
+    best_of_both_worlds.build()

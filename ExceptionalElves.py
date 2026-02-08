a5b65d89c74d0fc45d31a4b7c0a30e580a76cc02
Justin Elliott <justin@elliott.cc>
2026-02-08 16:41:39 -0500

    Additional racial features.

--- ExceptionalElves.py
+++ ExceptionalElves.py
@@ -6,7 +6,7 @@ Generates files for Exceptional Elves, a mod to give Elves and Half-Elves bonus 
 from functools import cache, cached_property
 import os
 
-from moddb import Bolster, Knowledge
+from moddb import Bolster, Knowledge, Movement
 from modtools.gamedata import PassiveData
 from modtools.lsx.game import (
     CharacterAbility,
@@ -150,6 +150,56 @@ class ExceptionalElves(Replacer):
         return name
 
     @cached_property
+    def _artful_dodger(self) -> str:
+        name = self.make_name("ArtfulDodger")
+        self.loca[f"{name}_DisplayName"] = "Artful Dodger"
+        self.add(PassiveData(
+            name,
+            using="UncannyDodge",
+            DisplayName=self.loca[f"{name}_DisplayName"],
+        ))
+        return name
+
+    @cached_property
+    def _cunning(self) -> str:
+        name = self.make_name("Cunning")
+        self.loca[f"{name}_DisplayName"] = "Cunning"
+        self.loca[f"{name}_Description"] = """
+            They'll never see you coming, until it's too late. As a bonus action, you can use
+            <LSTag Type="Spell" Tooltip="Shout_Dash_CunningAction">Cunning Action: Dash</LSTag>,
+            <LSTag Type="Spell" Tooltip="Shout_Hide_BonusAction">Cunning Action: Hide</LSTag>, or
+            <LSTag Type="Spell" Tooltip="Shout_Disengage_CunningAction">Cunning Action: Disengage</LSTag>.
+            """
+        self.add(PassiveData(
+            name,
+            DisplayName=self.loca[f"{name}_DisplayName"],
+            Description=self.loca[f"{name}_Description"],
+            Icon="Action_Hide",
+            Boosts=[
+                "UnlockSpell(Shout_Dash_CunningAction)",
+                "UnlockSpell(Shout_Hide_BonusAction)",
+                "UnlockSpell(Shout_Disengage_CunningAction)",
+            ],
+            Properties=["IsHidden"],
+        ))
+        return name
+
+    @cached_property
+    def _evasive(self) -> str:
+        name = self.make_name("Evasive")
+        self.loca[f"{name}_DisplayName"] = "Evasive"
+        self.add(PassiveData(
+            name,
+            using="Evasion",
+            DisplayName=self.loca[f"{name}_DisplayName"],
+        ))
+        return name
+
+    @cached_property
+    def _fleet_of_foot(self) -> str:
+        return Movement(self.mod).add_fast_movement(3.0, "Fleet of Foot")
+
+    @cached_property
     def _mistwalker(self) -> str:
         name = self.make_name("Mistwalker")
         self.loca[f"{name}_DisplayName"] = "Mistwalker"
@@ -165,9 +215,9 @@ class ExceptionalElves(Replacer):
         return name
 
     @cached_property
-    def _pickpocket(self) -> str:
-        name = self.make_name("Pickpocket")
-        self.loca[f"{name}_DisplayName"] = "Pickpocket"
+    def _light_fingered(self) -> str:
+        name = self.make_name("LightFingered")
+        self.loca[f"{name}_DisplayName"] = "Light-Fingered"
         self.loca[f"{name}_Description"] = """
             You gain <LSTag Tooltip="Expertise">Expertise</LSTag> in
             <LSTag Tooltip="SleightOfHand">Sleight of Hand</LSTag>.
@@ -178,6 +228,94 @@ class ExceptionalElves(Replacer):
             Description=self.loca[f"{name}_Description"],
             Boosts=["ProficiencyBonus(Skill,SleightOfHand)", "ExpertiseBonus(SleightOfHand)"],
             Icon="Spell_Conjuration_MageHand",
+        ))
+        return name
+
+    @cached_property
+    def _quick_handed(self) -> str:
+        name = self.make_name("QuickHanded")
+        self.loca[f"{name}_DisplayName"] = "Quick-Handed"
+        self.add(PassiveData(
+            name,
+            using="FastHands",
+            DisplayName=self.loca[f"{name}_DisplayName"],
+        ))
+        return name
+
+    @cached_property
+    def _resilient(self) -> str:
+        name = self.make_name("Resilient")
+        self.loca[f"{name}_DisplayName"] = "Resilient"
+        self.loca[f"{name}_Description"] = """
+            You shrug off attacks, taking 2 less damage from all sources.
+            """
+        self.add(PassiveData(
+            name,
+            DisplayName=self.loca[f"{name}_DisplayName"],
+            Description=self.loca[f"{name}_Description"],
+            Boosts=["DamageReduction(All,Flat,2)"],
+            Icon="PassiveFeature_UnarmoredDefense_Barbarian",
+        ))
+        return name
+
+    @cached_property
+    def _sure_footed(self) -> str:
+        name = self.make_name("SureFooted")
+        self.loca[f"{name}_DisplayName"] = "Sure-Footed"
+        self.loca[f"{name}_Description"] = """
+            You have become an expert at moving through the wilderness.
+            <LSTag Type="Status" Tooltip="DIFFICULT_TERRAIN">Difficult Terrain</LSTag> no longer slows you down.
+            You can't be Enwebbed, Entangled, or Ensnared and can't slip on grease or ice.
+            """
+        self.add(PassiveData(
+            name,
+            DisplayName=self.loca[f"{name}_DisplayName"],
+            Description=self.loca[f"{name}_Description"],
+            Boosts=[
+                "StatusImmunity(SG_DifficultTerrain)",
+                "StatusImmunity(SHADOW_CURSED_VINES)",
+                "StatusImmunity(SPIKE_GROWTH)",
+                "StatusImmunity(BLIGHT_ENTANGLE)",
+                "StatusImmunity(PLANT_GROWTH)",
+                "StatusImmunity(DIFFICULT_TERRAIN_VINES)",
+                "StatusImmunity(DIFFICULT_TERRAIN_OVERGROWTH)",
+                "StatusImmunity(ENSNARED_VINES)",
+                "StatusImmunity(TWN_ARABELLAPOWERS_ARABELLAVINES)",
+                "StatusImmunity(WEB)",
+                "StatusImmunity(ENSNARED)",
+                "StatusImmunity(PRONE_GREASE)",
+                "StatusImmunity(PRONE_ICE)",
+                "StatusImmunity(ENSNARING_STRIKE)",
+                "StatusImmunity(ENSNARING_STRIKE_2)",
+                "Tag(PLANT_IMPEDE_ADV)",
+            ],
+            Icon="PassiveFeature_LandsStride_DifficultTerrain",
+        ))
+        return name
+
+    @cached_property
+    def _swordmaster(self) -> str:
+        name = self.make_name("Swordmaster")
+        self.loca[f"{name}_DisplayName"] = "Swordmaster"
+        self.loca[f"{name}_Description"] = """
+            You are proficient with all swords, and gain a +2 bonus to
+            <LSTag Tooltip="MeleeWeaponAttack">melee weapon attacks</LSTag> and damage.
+            """
+        self.add(PassiveData(
+            name,
+            DisplayName=self.loca[f"{name}_DisplayName"],
+            Description=self.loca[f"{name}_Description"],
+            Boosts=[
+                "Proficiency(Shortswords)",
+                "Proficiency(Longswords)",
+                "Proficiency(Greatswords)",
+                "Proficiency(Rapiers)",
+                "Proficiency(Scimitars)",
+                "RollBonus(MeleeWeaponAttack,2)",
+                "RollBonus(MeleeOffHandWeaponAttack,2)",
+                "IF(IsMeleeWeaponAttack()):CharacterWeaponDamage(2)",
+            ],
+            Icon="PassiveFeature_FightingStyle_Archery",
         ))
         return name
 
@@ -196,18 +334,27 @@ class ExceptionalElves(Replacer):
     def _racial_passives(self) -> list[tuple[int, str, str]]:
         return sorted([
             ( 1, "Archer",              self._archer),
+            ( 1, "Light-Fingered",      self._light_fingered),
             ( 1, "Naturally Stealthy",  "Halfling_LightfootStealth"),
-            ( 1, "Pickpocket",          self._pickpocket),
+            ( 1, "Swordmaster",         self._swordmaster),
+            ( 3, "Cunning",             self._cunning),
+            ( 3, "Fleet of Foot",       self._fleet_of_foot),
             ( 3, "Jack of All Trades",  "JackOfAllTrades"),
+            ( 3, "Quick-Handed",        self._quick_handed),
+            ( 5, "Artful Dodger",       self._artful_dodger),
+            ( 5, "Extra Attack",        "ExtraAttack"),
             ( 5, "Mistwalker",          self._mistwalker),
+            ( 7, "Evasive",             self._evasive),
+            ( 7, "Resilient",           self._resilient),
+            ( 9, "Sure-Footed",         self._sure_footed),
             (11, "Well-Practiced",      self._well_practiced),
         ], key=lambda item: item[1])
 
     @cache
     def _racial_passive_list(self, progress: Progression) -> tuple[str, str]:
-        list_name = f"Racial Abilities Level {progress.Level}"
+        list_name = f"Racial Features Level {progress.Level}"
         list_uuid = self.make_uuid(list_name)
-        selector_id = self.make_name(f"{progress.Name}RacialAbilities_Level_{progress.Level}")
+        selector_id = self.make_name(f"{progress.Name}RacialFeatures_Level_{progress.Level}")
         
         if list_uuid not in self._passive_list_uuids:
             self.add(PassiveList(
@@ -228,12 +375,12 @@ class ExceptionalElves(Replacer):
             UUID=self.make_uuid(selector_id),
         ))
 
-        self.loca["RacialAbilities_DisplayName"] = f"Racial Abilities"
-        self.loca["RacialAbilities_Description"] = f"Select a racial ability."
+        self.loca["RacialFeatures_DisplayName"] = f"Racial Features"
+        self.loca["RacialFeatures_Description"] = f"Select a racial feature."
 
         self.add(ProgressionDescription(
-            DisplayName=self.loca["RacialAbilities_DisplayName"],
-            Description=self.loca["RacialAbilities_Description"],
+            DisplayName=self.loca["RacialFeatures_DisplayName"],
+            Description=self.loca["RacialFeatures_Description"],
             ProgressionTableId=progress.TableUUID,
             SelectorId=selector_id,
             UUID=self.make_uuid(f"{selector_id} Description"),

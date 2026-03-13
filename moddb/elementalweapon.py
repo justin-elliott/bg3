bb2980b6895fc3774aafdccc3c97998d0c0d438a
Justin Elliott <justin@elliott.cc>
2026-03-13 15:16:01 -0400

    Add weapon bond to elemental weapon.

--- moddb/elementalweapon.py
+++ moddb/elementalweapon.py
@@ -3,7 +3,7 @@
 Once per rest Elemental Weapon for Baldur's Gate 3 mods.
 """
 
-from modtools.gamedata import SpellData
+from modtools.gamedata import SpellData, StatusData
 from modtools.mod import Mod
 from typing import ClassVar
 
@@ -19,28 +19,66 @@ class ElementalWeapon:
 
     def add_elemental_weapon(self) -> str:
         name = self._mod.make_name("ElementalWeapon")
+        status_name = self._mod.make_name("ELEMENTAL_WEAPON")
+
+        self._mod.loca[f"{name}_Description"] = """
+            Imbue a weapon with elemental power. It receives a +[1] bonus to
+            <LSTag Tooltip="AttackRoll">Attack Rolls</LSTag>, and deals an additional 1d4 damage of your choice.
+            The weapon can't be knocked out of the wielder's hand, and automatically returns to the wielder when
+            <LSTag Type="Spell" Tooltip="Throw_Throw">Thrown</LSTag>.
+        """
         self._mod.add(SpellData(
             name,
             using="Target_ElementalWeapon",
+            Description=self._mod.loca[f"{name}_Description"],
             SpellType="Target",
             ContainerSpells=[f"{name}_{element}" for element in self._ELEMENTS],
             Cooldown="OncePerRestPerItem",
             UseCosts="ActionPoint:1",
             SpellFlags=["HasVerbalComponent", "HasSomaticComponent", "IsSpell", "IsMelee", "IsLinkedSpellContainer"],
         ))
+
+        self._mod.loca[f"{status_name}_Description"] = """
+            Has a +[1] bonus to <LSTag Tooltip="AttackRoll">Attack Rolls</LSTag> and deals an additional [2].
+            The weapon can't be knocked out of the wielder's hand, and automatically returns to the wielder when
+            <LSTag Type="Spell" Tooltip="Throw_Throw">Thrown</LSTag>.
+        """
+
         for element in self._ELEMENTS:
+            self._mod.loca[f"{name}_{element}_Description"] = """
+                Imbue a weapon with elemental power. It receives a +[1] bonus to
+                <LSTag Tooltip="AttackRoll">Attack Rolls</LSTag>, and deals an additional [2].
+                The weapon can't be knocked out of the wielder's hand, and automatically returns to the wielder when
+                <LSTag Type="Spell" Tooltip="Throw_Throw">Thrown</LSTag>.
+            """
             self._mod.add(SpellData(
                 f"{name}_{element}",
                 using=f"Target_ElementalWeapon_{element}",
+                Description=self._mod.loca[f"{name}_{element}_Description"],
+                DescriptionParams=["1", f"DealDamage(1d4,{element})"],
                 SpellType="Target",
                 SpellContainerID=name,
                 SpellProperties=[
-                    f"IF(Item()):ApplyStatus(ELEMENTAL_WEAPON_{element.upper()},100,-1)",
-                    f"IF(not Item()):ApplyEquipmentStatus(MainHand,ELEMENTAL_WEAPON_{element.upper()},100,-1)",
-                    f"IF(not Item()):ApplyEquipmentStatus(OffHand,ELEMENTAL_WEAPON_{element.upper()},100,-1)",
+                    f"IF(Item()):ApplyStatus({status_name}_{element.upper()},100,-1)",
+                    f"IF(not Item()):ApplyEquipmentStatus(MainHand,{status_name}_{element.upper()},100,-1)",
+                    f"IF(not Item()):ApplyEquipmentStatus(OffHand,{status_name}_{element.upper()},100,-1)",
                 ],
                 Cooldown="OncePerRestPerItem",
                 UseCosts="ActionPoint:1",
                 SpellFlags=["HasVerbalComponent", "HasSomaticComponent", "IsSpell", "IsMelee"],
             ))
+
+            self._mod.add(StatusData(
+                f"{status_name}_{element.upper()}",
+                using=f"ELEMENTAL_WEAPON_{element.upper()}",
+                StatusType="BOOST",
+                Description=self._mod.loca[f"{status_name}_Description"],
+                Boosts=[
+                    "CannotBeDisarmed()",
+                    "ItemReturnToOwner()",
+                    "WeaponAttackRollBonus(1)",
+                    f"WeaponDamage(1d4,{element})",
+                ]
+            ))
+
         return name
